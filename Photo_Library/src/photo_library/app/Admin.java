3945403fab703a78528f46ca0ea953bb247dc0c7
akhan2025 <ahk21170@gmail.com>
2021-11-14 11:31:09 -0500

    added admin/user/stock functionality

--- Photo_Library/src/photo_library/app/Admin.java
+++ Photo_Library/src/photo_library/app/Admin.java
@@ -1,5 +1,71 @@
 package photo_library.app;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Admin {
-
+	ArrayList<User> users;
+	
+	public Admin() throws Exception {
+		File user_file = new File("users.txt");
+		if(user_file.exists()) {
+			Scanner sc = new Scanner(user_file);
+			while(sc.hasNextLine()) {
+				User user = new User(sc.nextLine());
+				users.add(user);
+			}
+			sc.close();
+		}
+	}
+	
+	public ArrayList<User> getUsers(){
+		return users;
+	}
+	
+	public boolean deleteUser(String user) throws IOException {
+		User temp = null;
+		for(User u: users) {
+			if(u.getUser().equals(user)) {
+				temp = u;
+				break;
+			}
+		}
+		if(temp != null) {
+			users.remove(temp);
+			PrintWriter writer = new PrintWriter(new FileWriter("users.txt"));
+			for(User u: users) {
+				writer.println(u.getUser());
+			}
+			writer.close();
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean addUser(String user) throws Exception {
+		User temp = null;
+		for(User u: users) {
+			if(u.getUser().equals(user)) {
+				temp = u;
+				break;
+			}
+		}
+		if(temp != null) {
+			return false;
+		}
+		else {
+			users.add(new User(user));
+			PrintWriter writer = new PrintWriter(new FileWriter("users.txt"));
+			for(User u: users) {
+				writer.println(u.getUser());
+			}
+			writer.close();
+			return true;
+		}
+	}
 }
--- Photo_Library/src/photo_library/app/User.java
+++ Photo_Library/src/photo_library/app/User.java
@@ -1,5 +1,41 @@
 package photo_library.app;
 
-public class User {
 
+import java.util.HashMap;
+import java.io.File;
+import java.util.Scanner;
+
+
+public class User {
+	private String username;
+	private HashMap<String, String> albums;
+	public User(String username) throws Exception {
+		this.username = username;
+		File file = new File(username+".txt");
+		if(file.exists()) {
+			Scanner sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				String temp = sc.nextLine();
+				String name = temp.split(" ")[0];
+				String loc = temp.split(" ")[1];
+				addAlbum(name, loc);
+			}
+			sc.close();
+		}
+		else {
+			file.createNewFile();
+		}
+	}
+	
+	public String getUser(){
+		return username;
+	}
+	
+	public void addAlbum(String name, String loc){
+		albums.put(name, loc);
+	}
+	
+	public  HashMap<String, String> getAlbums() {
+		return albums;
+	}
 }
